64a1ffa51994e239e2863b62ec9db86d577eab11
Camilo Contreras <contreras.camilo32@gmail.com>
2019-08-01 12:25:26 -0400

    avances ses

---
+++ FUENTES/JAVA/QuartzTest/src/test/java/cl/quartz/testing/QuartzTest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package cl.quartz.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author ccontrerasc
+ *
+ */
+@SpringBootTest
+public class QuartzTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
